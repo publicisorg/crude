456c3622346f58cec4a05ac93231c08dad74be5d
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-05 12:28:28 -0300

    cambio iconos

--- src/components/common/items/index.tsx
+++ src/components/common/items/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const styles = "flex flex-col justify-center items-center text-xl font-bold text-black p-4 border rounded shadow-md duration-300 bg-white hover:bg-gray-200 scale-100 hover:scale-105 cursor-pointer";
-const iconStyles = "w-48 fill-transparent stroke-gray-400";
+const iconStyles = "w-48";
 
 export function ItemFolder(props: any) {
 
@@ -62,7 +62,7 @@ export function ItemFrame(props: any) {
                 <iframe className="bg-white rounded-xl p-4 w-2/3 h-4/5" src={'https://desarrollodesitios0.site/2023/newsletters/' + props.path.join('/') + '/' + props.label} />
                 <span onClick={() => openEditor(true)} className="text-black bg-white rounded-md cursor-pointer flex justify-center px-2 py-1 items-center text-lg drop-shadow-xl duration-200  hover:bg-gray-200">Editar</span>
                 <span onClick={handleClick} className="text-white bg-red-500 rounded-full top-4 right-4 cursor-pointer w-10 h-10 flex justify-center items-center text-3xl absolute drop-shadow-xl duration-200 hover:text-red-500 hover:bg-white">X</span>
-            </div>} 
+            </div>}
             {editor && <video className="fixed z-[99999] w-full inset-0" src="https://desarrollodesitios0.site/crude/tutorial.mp4" autoPlay loop></video>}
         </>
     )
@@ -73,10 +73,8 @@ export function ItemNotSupported(props: any) {
     return (
         <>
             <a href={'https://desarrollodesitios0.site/2023/newsletters/' + props.path.join('/') + '/' + props.label} download={'https://desarrollodesitios0.site/2023/newsletters/' + props.path.join('/') + '/' + props.label} className={styles} key={props.index}>
-                <svg className={iconStyles} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M2 20V4C2 3.44772 2.44772 3 3 3H8.44792C8.79153 3 9.11108 3.17641 9.29416 3.46719L10.5947 5.53281C10.7778 5.82359 11.0974 6 11.441 6H21C21.5523 6 22 6.44772 22 7V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20Z" />
-                    <path d="M10.1833 11.5697C10.2383 11.1301 10.4368 10.7209 10.7481 10.4056C11.0594 10.0903 11.466 9.8865 11.9049 9.82585C12.3438 9.7652 12.7904 9.85107 13.1755 10.0701C13.5607 10.2892 13.8627 10.6292 14.0349 11.0375C14.2071 11.4457 14.2397 11.8994 14.1278 12.3281C14.0159 12.7568 13.7656 13.1365 13.4159 13.4085C13.2977 13.5004 13.1508 13.5782 12.991 13.6411C12.4794 13.8425 12.0212 14.2761 12.0212 14.8259L12.0212 14.9733" />
-                    <circle cx="12.0212" cy="17.2707" r="0.91897" fill="#200E32" />
+                <svg className={iconStyles} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.5 13C12.5 11 14 11.5 14 10C14 9.34375 13.5 8.5 12.5 8.5C11.5 8.5 11 9 10.5 9.5M12.5 16V14.5M5.5 5.5H19.5V19.5H5.5V5.5Z" stroke="#121923" stroke-width="1.2" />
                 </svg>
                 {props.label}
             </a>
