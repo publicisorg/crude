f88520e005b0ea73de5a8bba0f302d69405505a7
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-17 11:05:37 -0300

    menu corregido

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import SheetData from './components/common/sheetdata/SheetData';
 import Table from './components/containers/table'
+import { ButtonToggle } from './components/common/buttons';
+import Folders from './components/containers/folders';
 
 function App() {
 
@@ -9,11 +11,12 @@ function App() {
 
   return (
     <main className="">
-      <div className="flex flex-row m-8 gap-8">
-        {/*<ButtonToggle actualValue={type} function={setType} arguments={"folders"} label="Carpetas" />*/}
-        {/*<ButtonToggle actualValue={type} function={setType} arguments={"db"} label="Base de datos" />*/}
+      <div className="flex flex-row m-8 gap-8 justify-center items-center">
+        {<ButtonToggle actualValue={type} function={setType} arguments={"folders"} label="Carpetas" />}
+        {<ButtonToggle actualValue={type} function={setType} arguments={"db"} label="Base de datos" />}
+        {<ButtonToggle actualValue={type} function={setType} arguments={"sheet"} label="Status" />}
       </div>
-      {/*type == "folders" && <Folders />*/}
+      {type == "folders" && <Folders />}
       {type == "db" && <Table />}
       {type == "sheet" && 
       
