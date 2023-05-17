194765de297e9e0d6eb076faecfdfc19f5aa591e
jumperning <maxy.m.rodriguez@gmail.com>
2023-05-17 20:15:47 -0300

    sidebar new

--- src/components/containers/aside-menu/index.tsx
+++ src/components/containers/aside-menu/index.tsx
@@ -12,19 +12,23 @@ function MenuAside(props:any) {
 
     return (
         <aside className="h-screen fixed gap-4 bg-black/5 dark:bg-white/5 w-80 border-r border-black/25 dark:border-white/25 py-4 flex flex-col justify-between items-center">
-            <img className="h-28" src="ThePub-Black.png" />
+            <img className="h-28 mt-8" src="ThePub-Black.png" />
             
-            <div className="flex flex-col justify-center items-center w-full gap-1 px-4">
+            <div className="flex flex-col justify-center items-center w-full gap-1 px-4 ">
                 <MenuButton function={setMenuSelected} arguments="desktop" selected={menuSelected}><AiOutlineHome/>Escritorio</MenuButton>
                 <MenuButton function={setMenuSelected} arguments="tasks" selected={menuSelected}><AiOutlineContainer/>Tareas</MenuButton>
                 <MenuButton function={setMenuSelected} arguments="folders" selected={menuSelected}><AiOutlineFolder/>Carpetas</MenuButton>
                 <MenuButton function={setMenuSelected} arguments="databases" selected={menuSelected}><AiOutlinePieChart/>Bases de Datos</MenuButton>
             </div> 
 
-            <div className="flex justify-center flex-col items-center">
-                <p>Hola Julian</p>
-                <p>Perfil</p>
-                <p>Cerrar Sesion</p>
+            <div className="flex justify-start items-center gap-5 mb-4 pt-8 border-gray-800  px-6  border-t-2 w-full">
+                <div>
+                    <img src="./profile.png" alt="Profile" className="w-12 rounded-full" />
+                </div>
+                <div>
+                <p>Hola Julian!</p>
+               <a href="#"><small className="text-gray-500">Cerrar Sesion</small></a> 
+                </div>
             </div>
 
         </aside>
