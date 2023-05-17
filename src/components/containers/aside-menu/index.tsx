6b66630cacce4c403085e3e90aca4b7da857928a
jumperning <maxy.m.rodriguez@gmail.com>
2023-05-17 20:59:39 -0300

    sidebar 3

--- src/components/containers/aside-menu/index.tsx
+++ src/components/containers/aside-menu/index.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import MenuButton from "./complementary/menu-buttons"
-import { AiOutlineHome, AiOutlineContainer,AiOutlineFolder,AiOutlinePieChart } from "react-icons/ai";
+import { AiOutlineHome, AiOutlineContainer, AiOutlineFolder, AiOutlinePieChart } from "react-icons/ai";
 
-function MenuAside(props:any) {
+function MenuAside(props: any) {
 
     const [menuSelected, setMenuSelected] = useState("desktop");
 
@@ -11,29 +11,30 @@ function MenuAside(props:any) {
     }, [menuSelected])
 
     return (
-        <aside className="h-screen fixed gap-2 bg-black/5 dark:bg-white/5 w-80 border-r border-black/25 dark:border-white/25  flex flex-col justify-between items-center">
+        <aside className="h-screen fixed gap-2 bg-[#1f2128] dark:bg-gradient-to-b from-[#1F1F1F] to-[#1f2128] w-80 border-r border-[#434343] dark:border-white/25  flex justify-between flex-col  items-center">
+            <div className="flex flex-col justify-evenly">
             <div className="pb-8 px-6  w-full flex justify-center">
-                <div className="bg-white/5 mt-9 w-full flex justify-center items-center p-2">
-            <img className="w-1/2" src="ThePub-Black.png" />
-            </div>
+                <div className="mt-9 w-full flex justify-center items-center p-2">
+                    <img className="w-1/2" src="ThePub-Black.png" />
+                </div>
             </div>
             <div className="flex flex-col justify-center items-center w-full gap-1 px-4 ">
-                <MenuButton function={setMenuSelected} arguments="desktop" selected={menuSelected}><AiOutlineHome/>Escritorio</MenuButton>
-                <MenuButton function={setMenuSelected} arguments="tasks" selected={menuSelected}><AiOutlineContainer/>Tareas</MenuButton>
-                <MenuButton function={setMenuSelected} arguments="folders" selected={menuSelected}><AiOutlineFolder/>Carpetas</MenuButton>
-                <MenuButton function={setMenuSelected} arguments="databases" selected={menuSelected}><AiOutlinePieChart/>Bases de Datos</MenuButton>
-            </div> 
-
+                <MenuButton function={setMenuSelected} arguments="desktop" selected={menuSelected}><AiOutlineHome />Escritorio</MenuButton>
+                <MenuButton function={setMenuSelected} arguments="tasks" selected={menuSelected}><AiOutlineContainer />Tareas</MenuButton>
+                <MenuButton function={setMenuSelected} arguments="folders" selected={menuSelected}><AiOutlineFolder />Carpetas</MenuButton>
+                <MenuButton function={setMenuSelected} arguments="databases" selected={menuSelected}><AiOutlinePieChart />Bases de Datos</MenuButton>
+            </div>
+            </div>
             <div className="flex justify-start items-center gap-5  py-3 bg-white/5   px-6   w-full">
                 <div>
-                <div className="relative">
-                    <img alt="" className="rounded w-12 rounded"  src="./profile.png" />
-                    <span className="absolute h-3.5 w-3.5 rounded-full border-2 border-white dark:border-gray-800 bg-green-400 -top-1 -left-1"></span></div>
-                  
+                    <div className="relative">
+                        <img alt="" className="rounded w-12 rounded" src="./profile.png" />
+                        <span className="absolute h-3.5 w-3.5 rounded-full border-2 border-white dark:border-gray-800 bg-green-400 -top-1 -left-1"></span></div>
+
                 </div>
                 <div>
-                <p>Hola Julian!</p>
-               <a href="#"><small className="text-gray-500">Cerrar Sesion</small></a> 
+                    <p>Hola Julian!</p>
+                    <a href="#"><small className="text-gray-500">Cerrar Sesion</small></a>
                 </div>
             </div>
 
