d3813915ada8cb89585d498b7e6a0c1e81117266
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-17 10:31:08 -0300

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 import Table from './components/containers/table'
+import Folders from './components/containers/folders';
+import { ButtonToggle } from './components/common/buttons';
 
 function App() {
 
-  //const [type, setType] = useState("db");
-  const type = 'db';
+  const [type, setType] = useState("db");
 
   return (
     <main className="w-full flex flex-col justify-center items-center inset-0 bg-slate-100">
       <div className="flex flex-row m-8 gap-8">
-        {/*<ButtonToggle actualValue={type} function={setType} arguments={"folders"} label="Carpetas" />*/}
-        {/*<ButtonToggle actualValue={type} function={setType} arguments={"db"} label="Base de datos" />*/}
+        {<ButtonToggle actualValue={type} function={setType} arguments={"folders"} label="Carpetas" />}
+        {<ButtonToggle actualValue={type} function={setType} arguments={"db"} label="Base de datos" />}
       </div>
-      {/*type == "folders" && <Folders />*/}
+      {type == "folders" && <Folders />}
       {type == "db" && <Table />}
     </main>
   )
