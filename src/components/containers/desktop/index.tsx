3d76e9d5ee327fa2e11888b7edf3e22ebb53f392
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-05-19 14:22:16 -0300

    correccion supervisor

--- src/components/containers/desktop/index.tsx
+++ src/components/containers/desktop/index.tsx
@@ -57,27 +57,27 @@ export function DesktopSupervisor(props: any) {
     return (
         <div className="flex flex-col gap-8 justify-start items-center h-full py-8">
             <div className="flex flex-row justify-center items-center w-full h-1/3 gap-8 px-8">
-                <div className={`${containerStyle} w-1/4 h-full`}>
+                <div className={`${containerStyle} w-full h-full`}>
                     <p className={`${pStyle}`}>Sin iniciar</p>
                     <span className="w-full h-full flex justify-center items-center text-7xl">{notStarted}</span>
                 </div>
-                <div className={`${containerStyle} w-1/4 h-full`}>
+                <div className={`${containerStyle} w-full h-full`}>
                     <p className={`${pStyle}`}>En Proceso</p>
                     <span className="w-full h-full flex justify-center items-center text-7xl">{inProcess}</span>
                 </div>
-                <div className={`${containerStyle} w-1/4 h-full`}>
+                <div className={`${containerStyle} w-full h-full`}>
                     <p className={`${pStyle}`}>Completadas</p>
                     <span className="w-full h-full flex justify-center items-center text-7xl">{done}</span>
                 </div>
             </div>
             <div className="flex flex-row justify-center items-center w-full h-2/3 gap-8 px-8">
-                <div className={`${containerStyle} w-[45%] p-4 rounded-2xl flex flex-col h-full`}>
-                    <p className={`${pStyle}`}>Tus tareas</p>
+                <div className={`${containerStyle} w-full p-4 rounded-2xl flex flex-col h-full`}>
+                    <p className={`${pStyle}`}>Tareas del Equipo</p>
                     <div className="w-full h-full mt-6 rounded-2xl overflow-y-auto">
                         <SheetData fullscreen={false} user={props.user} getInProcess={setInProcess} getDone={setDone} getNotStarted={setNotStarted}/>
                     </div>
                 </div>
-                <div className={`${containerStyle} w-[45%] p-4 rounded-2xl flex flex-col h-full`}>
+                <div className={`${containerStyle} w-full p-4 rounded-2xl flex flex-col h-full`}>
                     <p className={`${pStyle}`}>Tareas sin asignar</p>
                     <div className="w-full h-full mt-6 rounded-2xl overflow-y-auto">
                         <SheetData fullscreen={false} user="none" />
