d8fd1ff844f1ff8d703a92483a6e0de557d8749a
jumperning <maxy.m.rodriguez@gmail.com>
2023-06-01 10:29:45 -0300

    hora en los posts

--- src/components/common/Cardstatus/index.tsx
+++ src/components/common/Cardstatus/index.tsx
@@ -5,6 +5,7 @@ import { AiOutlineHeart, AiOutlineLike } from "react-icons/ai";
 
 export function CardStatus(props: any) {
   const [messages, setMessages] = useState([]);
+  const [timeElapsed, setTimeElapsed] = useState("");
 
   useEffect(() => {
     async function getMessageById() {
@@ -22,15 +23,37 @@ export function CardStatus(props: any) {
     getMessageById();
   }, [props.userId]);
 
+  useEffect(() => {
+    if (messages.length > 0) {
+      const message = messages[0];
+      const createdAt = moment(message.created_at); 
+      const now = moment(); 
+      const duration = moment.duration(now.diff(createdAt));
+      const hoursElapsed = duration.asHours();
+
+      let timeText = "";
+      if (hoursElapsed < 1) {
+        timeText = "Hace menos de una hora";
+      } else if (hoursElapsed < 24) {
+        timeText = `Hace ${Math.floor(hoursElapsed)} horas`;
+      } else {
+        timeText = `Hace ${Math.floor(hoursElapsed / 24)} días`;
+      }
+
+      setTimeElapsed(timeText);
+    }
+  }, [messages]);
+
   return (
-    <div className=" mt-14">
+    <div className="mt-14">
       {messages.map((message: any) => (
         <div className="border border-gray-300 p-4 flex mb-4" key={message.created_at}>
           <img className="profile-image w-12 h-12 rounded-full mr-4" src={props.urlImg} alt="Profile" />
           <div className="status-content">
             <div className="flex">
-            <p className="message font-bold">{props.name}</p>
-            <p className="message font-sm font-normal text-gray-500">@{props.userNick}</p>
+              <p className="message font-bold">{props.name}</p>
+              <p className="message font-sm font-normal text-gray-500">@{props.userNick}</p>
+              <p className="message font-sm font-normal text-gray-500">{timeElapsed}</p>
             </div>
             <p className="message font-bold">{message.message}</p>
             <p className="like-count text-gray-500 flex gap-2 items-center"><AiOutlineHeart/>{message.like}</p>
--- src/components/containers/profile/index.tsx
+++ src/components/containers/profile/index.tsx
@@ -20,7 +20,7 @@ export const ProfileUsers = (props: any) => {
         try {
             await supabase
                 .from('statusProfile')
-                .insert({ uuid: props.userId, message: status, like: 0, show:true });
+                .insert({ uuid: props.userId, message: status, like: 0, show: true });
 
             console.log(props.userId);
         } catch (error) {
@@ -32,35 +32,45 @@ export const ProfileUsers = (props: any) => {
     return (
         <main>
 
+
+
+
+
+
             <div className="container mx-auto p-6 ">
-                <div className="relative h-48 bg-cover bg-center" style={{ backgroundImage: "url('https://fotos.perfil.com/2022/07/14/trim/1280/720/ciudad-de-buenos-aires-1386884.jpg')" }}></div>
-                <div className="flex justify-center mt-[-4rem] relative">
-                    <div className="w-32 h-32 border-4 border-white rounded-full overflow-hidden z-10">
-                        <img className="w-full h-full object-cover" src={props.urlImg} alt={props.name} />
-                        <span className={props.active ? 'isActive' : 'inactive' + 'absolute z-10 top-0'}></span>
+                <div className='card'>
+                    <div className="card-body">
+                    <div className="relative h-48 bg-cover bg-center" style={{ backgroundImage: "url('https://fotos.perfil.com/2022/07/14/trim/1280/720/ciudad-de-buenos-aires-1386884.jpg')" }}></div>
+                    <div className="flex justify-center mt-[-4rem] relative">
+                        <div className="w-32 h-32 border-4 border-white rounded-full overflow-hidden z-10">
+                            <img className="w-full h-full object-cover" src={props.urlImg} alt={props.name} />
+                            <span className={props.active ? 'isActive' : 'inactive' + 'absolute z-10 top-0'}></span>
+                        </div>
+                    </div>
+                    <div className="mt-6 text-center">
+
+                        <h2 className='font-bold text-xl'>{props.name} {props.lastName}</h2>
+                        <h3> {props.occupation}</h3>
+                        <p className='text-gray-500'>@{props.userNick}</p>
+                    </div>
                     </div>
                 </div>
-                <div className="mt-6 text-center">
 
-                    <h2 className='font-bold text-xl'>{props.name} {props.lastName}</h2>
-                    <h3> {props.occupation}</h3>
-                    <p className='text-gray-500'>@{props.userNick}</p>
-                </div>
 
-                <div className="mt-6">
+                <div className="card mt-6">
                     <textarea value={status} onChange={handleStatusChange} className="w-full h-24 p-2 border border-gray-300 rounded resize-none text-black" placeholder={'¿Qué estás pensando, ' + props.name} />
 
 
                     <button
                         type="button"
-                        className="py-2 px-3 text-sm font-medium   focus:outline-none bg-white/10 rounded-lg border border-gray-200/10 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-white/10/10 dark:  dark:border-gray-600/10 dark:hover:  dark:hover:bg-gray-700"
+                        className="mt-4 py-2 px-3 text-sm font-medium   focus:outline-none bg-white/10 rounded-lg border border-gray-200/10 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-white/10/10 dark:  dark:border-gray-600/10 dark:hover:  dark:hover:bg-gray-700"
                         onClick={handleFormSubmit}
                     >
                         Compartir estado
                     </button>
 
                 </div>
-                <CardStatus userId={props.userId}  name={props.name} userNick={props.userNick} lastName={props.lastname} urlImg={props.urlImg} />
+                <CardStatus userId={props.userId} name={props.name} userNick={props.userNick} lastName={props.lastname} urlImg={props.urlImg} />
             </div>
         </main>
     )
