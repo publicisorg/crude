1a116b8984810558121a4131dd4a4273dbd1a0d6
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-06-06 13:16:37 -0300

    profiles;

--- src/components/containers/profile/ProfilePublic.tsx
+++ src/components/containers/profile/ProfilePublic.tsx
@@ -14,46 +14,46 @@ export const ProfileUsersPublic = () => {
     const [userId, setUserid] = useState("");
     const [bgColor2, setBgColor] = useState("");
     const [BgsecondaryColor, setBgsecondaryColor] = useState("");
-    
+
     const [borderColor, setborderColor] = useState("");
     const [fontColor, setfontColor] = useState("");
 
 
     //const [status, setStatus] = useState("");
 
-    const {userNick} = useParams()
+    const { userNick } = useParams()
 
 
-  if (!userNick) {
-    return <p>Cargando...</p>;
-  }
+    if (!userNick) {
+        return <p>Cargando...</p>;
+    }
 
 
-  async function getTaskById(userNick: any) {
-    const data: any = await supabase.from('users').select('*').eq("userNick", userNick);
-    return data;
-  }
-  getTaskById(userNick).then((element: any) => {
-    setUrlImg(element.data[0].urlImg);
-    setUrlImgPortada(element.data[0].urlImgPortada);
-    setName(element.data[0].name);
-    setLastName(element.data[0].lastName);
-    setOccupation(element.data[0].occupation);
-    setActive(element.data[0].active);
-    setActive(element.data[0].active);
-    //setStatus(element.data[0].status);
-    setUserid(element.data[0].uuid);
+    async function getTaskById(userNick: any) {
+        const data: any = await supabase.from('users').select('*').eq("userNick", userNick);
+        return data;
+    }
+    getTaskById(userNick).then((element: any) => {
+        setUrlImg(element.data[0].urlImg);
+        setUrlImgPortada(element.data[0].urlImgPortada);
+        setName(element.data[0].name);
+        setLastName(element.data[0].lastname);
+        setOccupation(element.data[0].occupation);
+        setActive(element.data[0].active);
+        setActive(element.data[0].active);
+        //setStatus(element.data[0].status);
+        setUserid(element.data[0].uuid);
 
-    setBgColor(element.data[0].bgcolor)
-    setBgsecondaryColor(element.data[0].secondaryColor)
+        setBgColor(element.data[0].bgcolor)
+        setBgsecondaryColor(element.data[0].secondaryColor)
 
-    setborderColor(element.data[0].borderColor)
-    setfontColor(element.data[0].fontColor)
+        setborderColor(element.data[0].borderColor)
+        setfontColor(element.data[0].fontColor)
 
-    
-  console.log(element.data[0])
-  })
-       
+
+        console.log(element.data[0])
+    })
+
 
 
     useEffect(() => {
@@ -77,27 +77,27 @@ export const ProfileUsersPublic = () => {
             console.error('Error al actualizar el estado:', error);
         }
     };*/
-    
-  
-    return (
-        <main className="h-screen"  style={{backgroundColor: bgColor2, color: fontColor,borderColor: borderColor}}>
 
-            <div className="container mx-auto p-6 ">
-                <div className="relative h-48 bg-cover bg-center" style={{ backgroundImage: "url("+urlImgPortada+")" }}></div>
-                <div className="flex justify-center mt-[-4rem] relative">
-                    <div className={`w-32 h-32 border-4 border-[`+borderColor+`] rounded-full overflow-hidden z-10`}>
-                        <img className="w-full h-full object-cover" src={urlImg} alt={name} />
-                        <span className={active ? 'isActive' : 'inactive' + 'absolute z-10 top-0'}></span>
+
+    return (
+        <main className="h-screen overflow-y-auto" style={{ backgroundColor: bgColor2, color: fontColor, borderColor: borderColor }}>
+            <div className="container mx-auto p-6">
+                <div className='border rounded-lg overflow-hidden pb-6 bg-white/10 mb-4' style={{ borderColor: borderColor }}>
+                    <div className="relative h-48 bg-cover bg-center" style={{ backgroundImage: "url(" + urlImgPortada + ")" }}></div>
+                    <div className="flex justify-center mt-[-4rem] relative">
+                        <div className={`w-32 h-32 border-4 rounded-full overflow-hidden z-10 shadow-lg`} style={{ borderColor: borderColor }}>
+                            <img className="w-full h-full object-cover" src={urlImg} alt={name} />
+                            <span className={active ? 'isActive' : 'inactive' + 'absolute z-10 top-0'}></span>
+                        </div>
+                    </div>
+                    <div className="mt-4 text-center">
+                        <h2 className='font-bold text-xl'>{name} {lastname}</h2>
+                        <h3> {occupation}</h3>
+                        <p className='text-gray-500'>@{userNick}</p>
                     </div>
                 </div>
-                <div className="mt-6 text-center">
 
-                    <h2 className='font-bold text-xl'>{name} {lastname}</h2>
-                    <h3> {occupation}</h3>
-                    <p className='text-gray-500'>@{userNick}</p>
-                </div>
-
-                <CardStatus userId={userId} borderColor={borderColor} BgsecondaryColor={BgsecondaryColor}  name={name} userNick={userNick} lastName={lastname} urlImg={urlImg} />
+                <CardStatus userId={userId} borderColor={borderColor} secondaryColor={BgsecondaryColor} name={name} userNick={userNick} lastName={lastname} urlImg={urlImg} />
             </div>
         </main>
     )
